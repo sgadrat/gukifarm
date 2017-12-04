c08eb8a8108f6093bbeba7c1e998999934d5a25a
Sylvain Gadrat <sgadrat@wontfix.it>
2017-12-04 11:22:30 +0100

    Makes music play on Chrome for Android
    Playing a music on mobile devices usually require a physical action from the user.
    Simply play it on first tap if not already playing since rtge initialization.

--- guki_farm/js/states/title.js
+++ guki_farm/js/states/title.js
@@ -31,6 +31,9 @@ var Title = {
 		this.worldClick = function(x, y) {
 			Guki.changeState('ingame');
 			document.getElementById('btn_click').play();
+			if (document.getElementById('music').paused) {
+				document.getElementById('music').play();
+			}
 		};
 
 		// Initialization logic
